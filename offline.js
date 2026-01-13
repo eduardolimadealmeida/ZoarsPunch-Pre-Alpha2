0319617aa634f4ee9d45e58bccf115ecb29483a3
eduardolimadealmeida <eduardolimadealmeida130411@gmail.com>
2026-01-13 09:29:27 -0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1768307258,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/solid.png",
+		"images/sprite-sheet0.png",
+		"images/sprite-sheet1.png",
+		"images/sprite-sheet2.png",
+		"images/sprite-sheet3.png",
+		"images/player1-sheet0.png",
+		"images/cam-sheet0.png",
+		"images/player2-sheet0.png",
+		"images/spritefont.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
